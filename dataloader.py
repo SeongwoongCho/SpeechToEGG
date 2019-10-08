fix a bug of normalization

dataloader.py
@@ -16,7 +16,7 @@ def process(t): ## args : drt, file,window,step
     X,y = [],[]
 
     x,sr = librosa.load(drt+file,sr=16000,mono=False)
-    itvs = librosa.effects.split(x[0],frame_length = int(window*0.75), hop_length = int(window*0.5),top_db = 25)
+    itvs = librosa.effects.split(x[0],frame_length = int(window*0.5), hop_length = int(window*0.25),top_db = 25)
     
     ## EGG와 Speech 모두 잡음성분 제거
 #     x[0] = butter_lowpass_filter(x[0],2500,16000)
@@ -66,7 +66,7 @@ def load_noise():
     for file in tqdm(os.listdir('./datasets/TrainData/normal_noise/')):
         tmp = np.array([])
         x,sr = librosa.load('./datasets/TrainData/normal_noise/' + file,sr=16000)
-        itvs = librosa.effects.split(x,frame_length = 128, hop_length = 64,top_db = 25)
+        itvs = librosa.effects.split(x,frame_length = 96, hop_length = 48,top_db = 25)
         for st,ed in itvs:
             tmp = np.concatenate((tmp,x[st:ed]))
         normal_noise.append(tmp.astype('float32'))

utils.py
@@ -17,9 +17,13 @@ def seed_everything(seed=1234):
 seed_everything(42)
 
 def check_data(x,y,threshold):
-    if(np.max(x) - np.min(x) < 0.1):
+    if(np.max(x[:192]) - np.min(x[:192]) < 0.1):
         return False
-    if(np.max(y)-np.min(y)<threshold):
+    if(np.max(y[:192])-np.min(y[:192])<threshold):
+        return False
+    if(np.max(x[-192:]) - np.min(x[-192:]) < 0.1):
+        return False
+    if(np.max(y[-192:])-np.min(y[-192:])<threshold):
         return False
     return True
 
@@ -101,13 +105,13 @@ def custom_aug(x,normal_noise,musical_noise):
             pi = random.randint(0,len(musical_noise)-1)
             pi2 = random.randint(0,len(musical_noise[pi])-193)
             y = musical_noise[pi][pi2:pi2+192]
-            if check_data(y,y,0.1):
+            if np.max(y)-np.min(y)>0.1:
                 y = normalize(y)
         else:
             pi = random.randint(0,len(normal_noise)-1)
             pi2 = random.randint(0,len(normal_noise[pi])-193)
             y = normal_noise[pi][pi2:pi2+192]
-            if check_data(y,y,0.1):
+            if np.max(y)-np.min(y)>0.1:
                 y = normalize(y)
     else:
         if p < 0.8: